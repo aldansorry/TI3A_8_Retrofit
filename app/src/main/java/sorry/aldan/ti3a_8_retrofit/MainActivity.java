76f2970c0d66f1974dd5c7a197d78e590365c423
Aldhan Biuzar Yahya <aldansorry@gmail.com>
2018-11-17 21:13:43 +0700

    init

---
+++ app/src/main/java/sorry/aldan/ti3a_8_retrofit/MainActivity.java
@@ -0,0 +1,72 @@
+package sorry.aldan.ti3a_8_retrofit;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import sorry.aldan.ti3a_8_retrofit.Adapters.ClickListener;
+import sorry.aldan.ti3a_8_retrofit.Adapters.MoviesAdapter;
+import sorry.aldan.ti3a_8_retrofit.Adapters.RecyclerTouchListener;
+import sorry.aldan.ti3a_8_retrofit.Models.Movie;
+import sorry.aldan.ti3a_8_retrofit.Models.MovieResponse;
+import sorry.aldan.ti3a_8_retrofit.Rest.ApiClient;
+import sorry.aldan.ti3a_8_retrofit.Rest.ApiInterface;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private final static String API_KEY = "51f9dddc116bca6a16250b1a0640d8f2";
+    List<Movie> movies;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (API_KEY.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Please obtain your API KEY first from themoviedb.org", Toast.LENGTH_LONG).show();
+            return;
+
+        }
+        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.movies_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
+        Call<MovieResponse> call = apiService.getTopRatedMovies(API_KEY);
+        call.enqueue(new Callback<MovieResponse>() {
+            @Override
+            public void onResponse(Call<MovieResponse>call, Response<MovieResponse> response) {
+                int statusCode = response.code();
+                movies = response.body().getResults();
+                recyclerView.setAdapter(new MoviesAdapter(movies, R.layout.list_item_movie, getApplicationContext()));
+                recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getApplicationContext(), recyclerView, new ClickListener() {
+                    @Override
+                    public void onClick(View view, final int position) {
+                        Intent i = new Intent(getApplicationContext(), DetailActivity.class);
+                        i.putExtra(DetailActivity.EXTRA_MOVIE,movies.get(position));
+                        startActivity(i);
+                        Toast.makeText(MainActivity.this, "Single Click on position        :"+position,
+                                Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onLongClick(View view, int position) {
+                        Toast.makeText(MainActivity.this, "Long press on position :"+position,
+                                Toast.LENGTH_LONG).show();
+                    }
+                }));
+            }
+            @Override
+            public void onFailure(Call<MovieResponse>call, Throwable t) {
+// Log error here since request failed
+                Log.e(TAG, t.toString());
+            }
+        });
+    }
+}
---
+++ app/src/main/java/sorry/aldan/ti3a_8_retrofit/Models/Movie.java
@@ -0,0 +1,67 @@
+package sorry.aldan.ti3a_8_retrofit.Models;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+public class Movie implements Serializable{
+    @SerializedName("overview")
+    private String overview;
+    @SerializedName("release_date")
+    private String releaseDate;
+    @SerializedName("id")
+    private Integer id;
+    @SerializedName("title")
+    private String title;
+    @SerializedName("vote_average")
+    private Double voteAverage;
+    @SerializedName("poster_path")
+    private String posterPath;
+    public Movie(String overview, String releaseDate, Integer id,
+                 String title, Double voteAverage,String poster) {
+        this.overview = overview;
+        this.releaseDate = releaseDate;
+        this.id = id;
+        this.title = title;
+        this.voteAverage = voteAverage;
+        this.posterPath = posterPath;
+    }
+    public String getOverview() {
+        return overview;
+    }
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public Double getVoteAverage() {
+        return voteAverage;
+    }
+    public void setVoteAverage(Double voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+}
